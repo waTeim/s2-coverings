7989454d0a68bcc0487e3c2e1609fd0c506a1af4
ThomasThelen <tommythelen@gmail.com>
2024-12-17 06:16:08 -0800

    Black code formatting

--- src/integrate.py
+++ src/integrate.py
@@ -4,7 +4,6 @@ import argparse
 
 from lib.integrator import Integrator
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
--- tests/test_kwg_ont.py
+++ tests/test_kwg_ont.py
@@ -12,5 +12,6 @@ def test_generate_cell_iri():
     """
     cell_id_int = 288230376151711744
     cell_id = S2CellId(cell_id_int)
-    assert generate_cell_iri(cell_id) == URIRef("http://stko-kwg.geog.ucsb.edu/lod/resource/s2.level1"
-                                                ".288230376151711744")
+    assert generate_cell_iri(cell_id) == URIRef(
+        "http://stko-kwg.geog.ucsb.edu/lod/resource/s2.level1" ".288230376151711744"
+    )
