fce697aa5977883dba67f2ed1104d61a029c0792
ThomasThelen <tommythelen@gmail.com>
2025-02-06 23:10:38 -0800

    Add unit tests for the region coverer

--- src/lib/geo/constrained_s2_region_converer.py
+++ src/lib/geo/constrained_s2_region_converer.py
@@ -8,7 +8,5 @@ class ConstrainedS2RegionCoverer(S2RegionCoverer):
 
     def __init__(self, min_level, max_level):
         super().__init__()
-        if max_level and max_level >= 0:
-            self.set_max_level(max_level)
-        if min_level and min_level >= 0:
-            self.set_min_level(min_level)
+        self.set_max_level(max_level)
+        self.set_min_level(min_level)
