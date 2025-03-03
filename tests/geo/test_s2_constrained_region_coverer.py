2b999dac80d022b7c203875fc369ed60d2a256e0
Thomas Thelen <tommythelen@gmail.com>
2025-03-02 23:02:05 -0800

    Revert "Support file chunking and add support for s2 coverings"

--- tests/geo/test_s2_constrained_region_coverer.py
+++
@@ -1,8 +0,0 @@
-from src.lib.geo.constrained_s2_region_converer import ConstrainedS2RegionCoverer
-
-def test_init():
-    max = 10
-    min = 9
-    converter = ConstrainedS2RegionCoverer(min, max)
-    assert int(converter.max_level()) == max
-    assert int(converter.min_level()) == min
